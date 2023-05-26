2144d1f7a821c73fb60c1f7a506548fa74706ac5
Sean Tedesco <stedesco@live.com>
2023-05-26 15:30:29 -0400

    program: i2c: use temporary i2c functionality in main program

---
+++ program/Inc/qset_i2c.h
@@ -0,0 +1,16 @@
+#include "stm32f4xx_hal.h"
+
+
+#define RECEIVE 0x2
+#define TRANSMISSION 0x3
+
+void bad_transfer_handle();// for you to initialize
+
+void i2c_pi_transfer(I2C_HandleTypeDef* hi2c, uint8_t* txBuffer,unsigned int txBufferSize, uint8_t* rxBuffer,unsigned int rxBufferSize);
+
+void bad_cmd_error();// for you to initialize
+
+void subsystem_task(int task); // for you to initialize
+
+
+
